012e1a28b4d8bb69797b627538c6b47c123703b2
Felipe Martins <6557756+fefas@users.noreply.github.com>
2021-05-12 23:00:51 +0200

    Remove unneeded code

--- src/Cli/Processor/CommandCollection.php
+++ src/Cli/Processor/CommandCollection.php
@@ -2,24 +2,20 @@
 
 namespace Bauhaus\Cli\Processor;
 
-use ArrayIterator;
 use Bauhaus\Cli\Command;
 use Bauhaus\Cli\Entrypoint;
 use Bauhaus\Cli\Input;
-use IteratorAggregate;
 
 /**
  * @internal
  */
-final class CommandCollection implements IteratorAggregate
+final class CommandCollection
 {
     private array $commands;
 
     private function __construct(Command ...$commands)
     {
         $this->commands = $commands;
-
-        $this->sort();
     }
 
     public static function fromEntrypoints(Entrypoint ...$entrypoints): self
@@ -32,11 +28,6 @@ final class CommandCollection implements IteratorAggregate
         return new self(...$commands);
     }
 
-    public function getIterator()
-    {
-        return new ArrayIterator($this->commands);
-    }
-
     public function findMatch(Input $input): ?Command
     {
         foreach ($this->commands as $command) {
@@ -46,13 +37,5 @@ final class CommandCollection implements IteratorAggregate
         }
 
         return null;
-    }
-
-    private function sort(): void
-    {
-        usort(
-            $this->commands,
-            fn (Command $a, Command $b) => strcmp($a->id(), $b->id()),
-        );
     }
 }
